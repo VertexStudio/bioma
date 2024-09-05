8d851e82e7868b25c34c19889f358f9ce61acb90
rozgo <alex.rozgo@gmail.com>
2024-09-05 15:59:17 -0600

    adds embeddings actor

--- bioma_llm/src/lib.rs
+++ bioma_llm/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod chat;
 pub mod rerank;
+pub mod embeddings;
 
 pub mod prelude {
     pub use crate::chat::{self, Chat};
     pub use ollama_rs::generation::chat::{ChatMessage, ChatMessageResponse};
     pub use crate::rerank::{self, Rerank, RankTexts, RankedText};
+    pub use crate::embeddings::{self, Embeddings, GenerateEmbeddings};
 }
