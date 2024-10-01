05fcd1ef93f2dc095b72d4a6c4773216275b911d
rozgo <alex.rozgo@gmail.com>
2024-09-30 21:40:11 -0600

    arrange docs, increase indexer chunk batch size

--- docs/curls.sh
+++ docs/curls.sh
@@ -10,7 +10,7 @@ cargo run --release -p bioma_llm --example rag_server
 curl -X POST http://localhost:8080/reset
 
 # Index some files:
-curl -X POST http://localhost:8080/index -H "Content-Type: application/json" -d '{"globs": ["/Users/rozgo/BiomaAI/bioma/bioma_*/**/*.rs"], "chunk_capacity": {"start": 500, "end": 2000}, "chunk_overlap": 200}'
+curl -X POST http://localhost:8080/index -H "Content-Type: application/json" -d '{"globs": ["/Users/rozgo/BiomaAI/bioma/bioma_*/**/*.rs"]}'
 
 # Retrieve context:
 curl -X POST http://localhost:8080/retrieve -H "Content-Type: application/json" -d '{"query": "Can I make a game with Bioma?", "threshold": 0.0, "limit": 10}'
