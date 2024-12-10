491bac893cee9314f6b91f834b46670969f492ad
Sergio <sergiorobayorr@gmail.com>
2024-12-10 10:35:28 -0600

    restore indexer

--- bioma_llm/examples/rag_server.rs
+++ bioma_llm/examples/rag_server.rs
@@ -626,7 +626,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         engine.clone(),
         indexer_id.clone(),
         Indexer::default(),
-        SpawnOptions::builder().exists(SpawnExistsOptions::Reset).build(),
+        SpawnOptions::builder().exists(SpawnExistsOptions::Restore).build(),
     )
     .await?;
 
