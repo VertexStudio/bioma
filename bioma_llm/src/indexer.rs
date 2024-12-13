872ee265c0df08b619ea0bfb486953421b85a984
Sergio <sergiorobayorr@gmail.com>
2024-12-13 16:58:11 -0600

    fix examples and tests

--- bioma_llm/src/indexer.rs
+++ bioma_llm/src/indexer.rs
@@ -1,5 +1,5 @@
 use crate::{
-    embeddings::{Embeddings, EmbeddingsError, StoreEmbeddings},
+    embeddings::{Embeddings, EmbeddingsError, ImageData, StoreEmbeddings},
     pdf_analyzer::{AnalyzePdf, PdfAnalyzer, PdfAnalyzerError},
 };
 use bioma_actor::prelude::*;
@@ -195,7 +195,7 @@ impl Indexer {
                 let result = ctx
                     .send_and_wait_reply::<Embeddings, StoreEmbeddings>(
                         StoreEmbeddings {
-                            content: EmbeddingContent::Image(vec![path_clone]),
+                            content: EmbeddingContent::Image(vec![ImageData::Path(path_clone)]),
                             metadata: metadata.map(|m| vec![m]),
                         },
                         embeddings_id,
