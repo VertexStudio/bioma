1674dcd20a8c4a2f8a8972d0c3dcdef4dd14719f
Raúl Granados <raul.granados12@gmail.com>
2024-12-18 16:10:40 -0600

    Adding markitdown python service

---
+++ assets/scripts/markitdown_script.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from markitdown import MarkItDown
+
+app = Flask(__name__)
+markitdown = MarkItDown()
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    try:
+        print("Received request", request)
+        file_path = request.form['file_path']
+        print("Got file", file_path)
+        result = markitdown.convert(file_path)
+        print("Converted file", result)
+        return jsonify({"text_content": result.text_content})
+    except Exception as e:
+        print("Error: ", e)
+        return jsonify({"error": str(e)})
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
