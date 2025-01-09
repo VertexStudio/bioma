0e7afa0110664b85b081d872df42f77b2e0f4bcf
Raúl Granados <raul.granados12@gmail.com>
2025-01-09 12:00:06 -0600

    Using defaults

--- tools/rag_server/src/request_schemas.rs
+++ tools/rag_server/src/request_schemas.rs
@@ -4,12 +4,16 @@ use utoipa::ToSchema;
 
 // /index Endpoint Schemas
 
+const DEFAULT_CHUNK_CAPACITY: std::ops::Range<usize> = 500..2000;
+const DEFAULT_CHUNK_OVERLAP: usize = 200;
+const DEFAULT_CHUNK_BATCH_SIZE: usize = 50;
+
 #[derive(ToSchema, Clone, Serialize, Deserialize)]
 pub struct IndexGlobsRequest {
     pub globs: Vec<String>,
-    pub chunk_capacity: ChunkCapacity,
-    pub chunk_overlap: usize,
-    pub chunk_batch_size: usize,
+    pub chunk_capacity: Option<ChunkCapacity>,
+    pub chunk_overlap: Option<usize>,
+    pub chunk_batch_size: Option<usize>,
 }
 
 #[derive(ToSchema, Clone, Serialize, Deserialize)]
@@ -20,10 +24,16 @@ pub struct ChunkCapacity {
 
 impl Into<IndexGlobs> for IndexGlobsRequest {
     fn into(self) -> IndexGlobs {
+        let chunk_capacity = match self.chunk_capacity {
+            Some(capacity) => capacity.start..capacity.end,
+            None => DEFAULT_CHUNK_CAPACITY,
+        };
+
         IndexGlobs::builder()
             .globs(self.globs)
-            .chunk_capacity(self.chunk_capacity.start..self.chunk_capacity.end)
-            .chunk_overlap(self.chunk_overlap)
+            .chunk_capacity(chunk_capacity)
+            .chunk_overlap(self.chunk_overlap.unwrap_or(DEFAULT_CHUNK_OVERLAP))
+            .chunk_batch_size(self.chunk_batch_size.unwrap_or(DEFAULT_CHUNK_BATCH_SIZE))
             .build()
     }
 }
