d9e10f6b1f09faa74bdcf7a83d3e90644ac0fcc6
Raúl Granados <raul.granados12@gmail.com>
2025-01-31 15:46:36 -0600

    Adding tools_actor arg and optional config

--- tools/cognition/src/client.rs
+++ tools/cognition/src/client.rs
@@ -1,6 +1,8 @@
+use std::path::PathBuf;
+
 use bioma_actor::prelude::*;
 use clap::Parser;
-use config::Args;
+use config::{Args as McpServerArgs, Config};
 use tool::ToolsHub;
 use tracing::info;
 use user::UserActor;
@@ -8,6 +10,12 @@ use user::UserActor;
 pub mod config;
 pub mod tool;
 pub mod user;
+
+#[derive(Parser)]
+pub struct Args {
+    pub tools_actor: String,
+    pub config: Option<PathBuf>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,7 +25,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().with_env_filter(filter).init();
 
     let args = Args::parse();
-    let config = args.load_config()?;
+
+    let config = match args.config {
+        Some(path) => {
+            let config = McpServerArgs { config: Some(path) };
+
+            config.load_config()?
+        }
+        None => Config::default(),
+    };
 
     // Initialize engine
     let engine = Engine::connect(config.engine.clone()).await?;
