2ec6d363b1eef36bbd06c910c4d4a3dea3362126
Raúl Granados <raul.granados12@gmail.com>
2025-02-06 14:53:32 -0600

    Usage descriptions

--- tools/cognition/src/server.rs
+++ tools/cognition/src/server.rs
@@ -363,6 +363,9 @@ async fn retrieve(body: web::Json<RetrieveContextRequest>, data: web::Data<AppSt
 #[utoipa::path(
     post,
     path = "/chat",
+    description =   "For the usage of this endpoint, and more specifically, for the 'tools' and 'tools_actor' field, only send of the two.<br>
+                    If you send 'tools', the definitions that you send, wil be send to the model and will return the tool call, without execute them.<br>
+                    If you send 'tools_actors', the endpoint with call the client that contains the tools and actually execute them.",
     request_body(content = ChatQueryRequestSchema, examples(
         ("Message only" = (summary = "Basic query", value = json!({
             "model": "llama3.2",
@@ -678,7 +681,10 @@ async fn chat(body: web::Json<ChatQueryRequestSchema>, data: web::Data<AppState>
 #[utoipa::path(
     post,
     path = "/think",
-    description = "Analyzes query and determines which tools to use and in what order.",
+    description =   "Analyzes query and determines which tools to use and in what order.<br>
+                    For the usage of this endpoint, and more specifically, for the 'tools' and 'tools_actor' field, only send of the two.<br>
+                    If you send 'tools', the definitions that you send, wil be send to the model and will return the tool call, without execute them.<br>
+                    If you send 'tools_actors', the endpoint with call the client that contains the tools and actually execute them.",
     request_body(content = ThinkQueryRequestSchema, examples(
         ("Message only" = (summary = "Basic query", value = json!({
             "messages": [
