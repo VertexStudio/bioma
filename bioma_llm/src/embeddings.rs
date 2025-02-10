29a5c3bf4ac86c4e297abc7fd143b6f627162e79
Sergio <sergiorobayorr@gmail.com>
2025-02-10 16:23:52 -0500

    modify query

--- bioma_llm/src/embeddings.rs
+++ bioma_llm/src/embeddings.rs
@@ -306,6 +306,7 @@ impl Message<TopK> for Embeddings {
             .bind(("prefix", self.table_prefix()))
             .await
             .map_err(SystemActorError::from)?;
+        println!("Results: {:?}", results);
         let results: Vec<Similarity> = results.take(0).map_err(SystemActorError::from)?;
         ctx.reply(results).await?;
         Ok(())
